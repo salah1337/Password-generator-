009c025299c93f1a17a2ee89676c4ea17ebaaefa
salah1337 <salah1337@protonmail.com>
2020-05-18 16:27:49 +0000

    JS update.

--- app.js
+++ app.js
@@ -20,3 +20,57 @@ const numbersOpt= document.getElementById('numbers');
 const symbolsOpt= document.getElementById('symbols');
 const generateOpt= document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
+
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultOpt.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
+
+generate.addEventListener('click', () => {
+	const length = +lengthOpt.value;
+	const hasLower = lowercaseOpt.checked;
+	const hasUpper = uppercaseOpt.checked;
+	const hasNumber = numbersOpt.checked;
+	const hasSymbol = symbolsOpt.checked;
+	
+	resultOpt.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+	let generatedPassword = '';
+	const typesCount = lower + upper + number + symbol;
+	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+	
+	// Doesn't have a selected type
+	if(typesCount === 0) {
+		return '';
+	}
+	
+	// create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += randomFunc[funcName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
